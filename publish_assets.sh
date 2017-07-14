64d81cb5b114fadb4ce89cc1c2c5d95465b5174f
Adrian Ghiuta <ady.ghiuta@gmail.com>
2017-07-15 00:37:57 +0300

    bump up python on Heroku to python 3

--- publish_assets.sh
+++ publish_assets.sh
@@ -3,5 +3,5 @@ env SECRET_KEY=`heroku config:get SECRET_KEY --app=eraya` \
     AWS_ACCESS_KEY_ID=`heroku config:get AWS_ACCESS_KEY_ID --app=eraya` \
     AWS_STORAGE_BUCKET_NAME=`heroku config:get AWS_STORAGE_BUCKET_NAME --app=eraya` \
     STATIC_ROOT='/vagrant/assets' \
-    python manage.py collectstatic --settings=eray.settings.heroku
+    python3 manage.py collectstatic --settings=eray.settings.heroku
 cd ..
--- run_dev_server.sh
+++ run_dev_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /vagrant_data
-/usr/bin/python manage.py migrate
+/usr/bin/python3 manage.py migrate
 
 printf "Installing requirements ...\n"
-pip install -r requirements/local.txt
+pip3 install -r requirements/local.txt
 
 printf "Running collectstatic ...\n"
-/usr/bin/python manage.py collectstatic --noinput --settings=eray.settings.local
+/usr/bin/python3 manage.py collectstatic --noinput --settings=eray.settings.local
 printf "Starting server on the box's port 8001 ...\n"
-/usr/bin/python manage.py runserver [::]:8001 --settings=eray.settings.local
+/usr/bin/python3 manage.py runserver [::]:8001 --settings=eray.settings.local
